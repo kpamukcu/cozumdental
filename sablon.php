d3cfa87937ac2294a793cce025860a14bc1c6671
kpamukcu <kapawebtasarim@gmail.com>
2021-10-18 22:21:27 +0300

    Bootstrap

---
+++ sablon.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap Css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Style Css -->
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <title>Document</title>
+</head>
+<body>
+
+<section id="menu" class="bg-info">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                Merhaba Dünya
+            </div>
+        </div>
+    </div>
+</section>
+    
+
+
+    <script src="js/jquery-3.5.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
