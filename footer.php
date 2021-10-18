fccdd01bea53e52f1680b129afc7eefd10da11fe
kpamukcu <kapawebtasarim@gmail.com>
2021-10-18 23:59:21 +0300

    Slider ve Şablon Yapı

---
+++ footer.php
@@ -0,0 +1,56 @@
+    <!-- Footer Section Start -->
+    <footer id="footer" class="bg-light py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 footer1">
+                    <h5 class="lead">Çözüm Dental</h5>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae nobis dolor atque.</p>
+                </div>
+                <div class="col-md-3 footer2">
+                    <h5 class="lead">Ürünlerimiz</h5>
+                    <a href="#">Ürün 1</a><br>
+                    <a href="#">Ürün 2</a><br>
+                    <a href="#">Ürün 3</a><br>
+                    <a href="#">Ürün 4</a><br>
+                </div>
+                <div class="col-md-3 footer3">
+                    <h5 class="lead">Hizmetlerimiz</h5>
+                    <a href="#">Hizmet 1</a><br>
+                    <a href="#">Hizmet 2</a><br>
+                    <a href="#">Hizmet 3</a><br>
+                    <a href="#">Hizmet 4</a><br>
+                </div>
+                <div class="col-md-3 footer4">
+                    <h5 class="lead">İletişim</h5>
+                    <i class="bi bi-geo-alt"></i> Lorem Ipsum Dolor Sit Amet <br>
+                    <i class="bi bi-telephone"></i> 0123 456 78 96 <br>
+                    <i class="bi bi-envelope-open"></i> info@cozumdental.com
+                </div>
+            </div>
+        </div>
+    </footer>
+    <section id="altFooter" class="bg-light pb-2">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 altFooter1">
+                    <small>Her Hakkı Sıklıdır &copy; Çözüm Dental</small>
+                </div>
+                <div class="col-md-4 text-right altFooter2">
+                    <small><a href="#" class="text-dark">Kullanıcı Sözleşmesi ve Gizlilik Politikası</a></small>
+                </div>
+                <div class="col-md-4 text-right altFooter3">
+                    <small><a href="#" class="text-dark">Web Tasarım</a></small>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer Section End -->
+
+
+
+    <script src="js/jquery-3.5.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap Css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Style Css -->
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <title>Çözüm Dental</title>
+</head>
+
+<body>
+
+    <!-- Header Section Start -->
+    <header id="menu">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="#">Çözüm Dental</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Ana Sayfa</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Kurumsal</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="urunlerimizMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Ürünlerimiz
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="urunlerimizMenuLink">
+                                        <a class="dropdown-item" href="#">Ürün 1</a>
+                                        <a class="dropdown-item" href="#">Ürün 2</a>
+                                        <a class="dropdown-item" href="#">Ürün 3</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="hizmetlerimizMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Hizmetlerimiz
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="hizmetlerimizMenuLink">
+                                        <a class="dropdown-item" href="#">Hizmet 1</a>
+                                        <a class="dropdown-item" href="#">Hizmet 2</a>
+                                        <a class="dropdown-item" href="#">Hizmet 3</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Markalar</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Referanslarımız</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">İletişim</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header Section End -->
