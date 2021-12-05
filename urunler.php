cb7c0b02a5bea42507ef16a3366947ac563e54b2
kpamukcu <kapawebtasarim@gmail.com>
2021-12-05 19:03:41 +0300

    ürünler sayfası

--- urunler.php
+++ urunler.php
@@ -1,4 +1,38 @@
 <?php require_once('header.php'); ?>
 
+<!-- Banner Section Start -->
+<section id="altSayfaBanner" class="py-15">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-white text-center">
+                <h2 class="lead display-4">İletişim</h2>
+                <a href="index.php" class="text-white">Ana Sayfa</a> / Ürünlerimiz
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Banner Section End -->
+
+<!-- Ürünler Content Section Start -->
+<section id="urunlerContent" class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <ul>
+                    <li>Diş Ünitleri</li>
+                    <li>Cerrahi Aspiratör</li>
+                    <li>Dental Ilok Işık ve Kamera</li>
+                </ul>
+            </div>
+            <div class="col-md-9">
+                <div class="row">
+                    Ürünler
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Ürünler Content Section End -->
+
 
 <?php require_once('footer.php') ?>
